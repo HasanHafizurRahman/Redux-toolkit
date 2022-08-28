5987e6700318b86b342dd90c0275663b6542f807
Hasan Shanto <hasanshanto922@gmail.com>
2022-08-28 21:36:00 +0600

    store

--- rtk/Feature/Counter/counterSlice.js
+++ rtk/Feature/Counter/counterSlice.js
@@ -9,6 +9,14 @@ const counterSlice = createSlice({
     name: "counter",
     initialState,
     reducers: {
-        
+        increment: (state, action) => {
+            state.count++;
+        },
+        decrement: (state, action) => {
+            state.count--;
+        }
     }
 })
+
+module.exports = counterSlice.reducer;
+module.exports.counterActions = counterSlice.actions;
